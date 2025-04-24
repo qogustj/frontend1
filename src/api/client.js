fac6bed576829b109a64fc0e12fc9ee645e0c8d1
배현서 <haulqogustj@naver.com>
2025-04-24 21:06:32 +0900

    push

--- src/api/client.js
+++ src/api/client.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 
 // axios 인스턴스 생성
 const client = axios.create({
-  baseURL: 'https://qogustj.shop/', // 백엔드 서버 URL (필요에 따라 수정)
+  baseURL: 'https://qogustj.shop', // 백엔드 서버 URL (필요에 따라 수정)
   headers: {
     'Content-Type': 'application/json',
   },
   // 서버 응답이 오래 걸리는 경우를 위해 타임아웃 설정 (30초)
-  timeout: 30000,
+  timeout: 60000,
 });
 
 export default client;
